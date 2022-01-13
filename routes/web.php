53476390bf28201638228e6e29ea4c9e1df840a2
dandisatrio <dandisatrio810@gmail.com>
2022-01-13 16:34:48 +0700

    Convert html to blade

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::prefix('admin')
     ->namespace('Admin')
